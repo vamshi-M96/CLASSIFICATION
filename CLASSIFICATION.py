4d1d656cf737259b273fa172e6edb80d2ce78583
vamshi-M96 <vamsi.meka96@gmail.com>
2025-06-15 11:54:15 +0530

    Update CLASSIFICATION.py

--- CLASSIFICATION.py
+++ CLASSIFICATION.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 import numpy as np
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.tree import  DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
